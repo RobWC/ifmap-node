b1dc74f66c0fa782569b193820fd0c1dc1cbbd67
Rob Cameron <rwcameron@gmail.com>
2012-05-09 17:20:11 -0700

    push, delete, and search protos working. Testing against OMAPD now as its the reference implementation

--- ifmap-node.js
+++ ifmap-node.js
@@ -1,12 +1,88 @@
-module.exports = new IfmapNode();
-
 var IfmapNode = function() {
-    this.info = '';
+  this.info = '';
 };
 
 IfmapNode.constructor = IfmapNode;
 
 IfmapNode.prototype.getSession = function() {
-    var message = '<?xml version="1.0"?><env:Envelope xmlns:env="http://www.w3.org/2003/05/soap-envelope" xmlns:ifmap="http://www.trustedcomputinggroup.org/2006/IFMAP/1"><env:Body><ifmap:new-session/> </env:Body></env:Envelope>'
-    return message;
+  var message = '<?xml version="1.0" encoding="UTF-8"?><SOAP-ENV:Envelope xmlns:SOAP-ENV="http://www.w3.org/2003/05/soap-envelope" xmlns:ifmap="http://www.trustedcomputinggroup.org/2006/IFMAP/1"><SOAP-ENV:Body><ifmap:new-session/> </SOAP-ENV:Body></SOAP-ENV:Envelope>';
+  return message;
+};
+
+IfmapNode.prototype.getUsers = function(sessionID) {
+  username = 'DrBeef';
+  var message = '<?xml version="1.0" encoding="utf-8"?>\
+  <SOAP-ENV:Envelope xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/" xmlns:SOAP-ENC="http://schemas.xmlsoap.org/soap/encoding/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:jnpr="http://www.juniper.net/2008/IFMAP/1" xmlns:meta="http://www.trustedcomputinggroup.org/2006/IFMAP-METADATA/1" xmlns:saml="urn:oasis:names:tc:SAML:2.0:assertion" xmlns:dsig="http://www.w3.org/2000/09/xmldsig#" xmlns:xenc="http://www.w3.org/2001/04/xmlenc#" xmlns:jnpr2="http://www.juniper.net/2009/IFMAP/1" xmlns:ifmap="http://www.trustedcomputinggroup.org/2006/IFMAP/1" xmlns:wsdl="http://www.juniper.net/2008/IFMAP/1/ifmap.wsdl">\
+    <SOAP-ENV:Header>\
+        <ifmap:session-id>' + sessionID + '</ifmap:session-id>\
+    </SOAP-ENV:Header>\
+    <SOAP-ENV:Body>\
+      <ifmap:search validation="None" match-links="(meta:username | meta:role)" max-depth="10">\
+        <identifier>\
+          <identity name="' + username + '" type="username"/>\
+        </identifier>\
+      </ifmap:search>\
+    </SOAP-ENV:Body>\
+</SOAP-ENV:Envelope>';
+  return message;
 }
+
+IfmapNode.prototype.deleteUser = function(sessionID,username) {
+  username = 'DrBeef';
+  var message = '<?xml version="1.0" encoding="utf-8"?>\
+  <SOAP-ENV:Envelope xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/" xmlns:SOAP-ENC="http://schemas.xmlsoap.org/soap/encoding/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:jnpr="http://www.juniper.net/2008/IFMAP/1" xmlns:meta="http://www.trustedcomputinggroup.org/2006/IFMAP-METADATA/1" xmlns:saml="urn:oasis:names:tc:SAML:2.0:assertion" xmlns:dsig="http://www.w3.org/2000/09/xmldsig#" xmlns:xenc="http://www.w3.org/2001/04/xmlenc#" xmlns:jnpr2="http://www.juniper.net/2009/IFMAP/1" xmlns:ifmap="http://www.trustedcomputinggroup.org/2006/IFMAP/1" xmlns:wsdl="http://www.juniper.net/2008/IFMAP/1/ifmap.wsdl">\
+    <SOAP-ENV:Header>\
+        <ifmap:session-id>' + sessionID + '</ifmap:session-id>\
+    </SOAP-ENV:Header>\
+    <SOAP-ENV:Body>\
+      <ifmap:publish xmlns:ifmap="http://www.trustedcomputinggroup.org/2006/IFMAP/1" xmlns:meta="http://www.trustedcomputinggroup.org/2006/IFMAP- METADATA/1">\
+      <delete>\
+        <identifier>\
+          <identity name="' + username + '" type="username" />\
+        </identifier>\
+      </delete>\
+    </ifmap:publish>\
+  </SOAP-ENV:Body>\
+</SOAP-ENV:Envelope>';
+  return message;
+}
+
+IfmapNode.prototype.setUser = function(sessionID, username) {
+  username = 'DrBeef';
+  var message = '<?xml version="1.0" encoding="utf-8"?>\
+  <SOAP-ENV:Envelope xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/" xmlns:SOAP-ENC="http://schemas.xmlsoap.org/soap/encoding/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:jnpr="http://www.juniper.net/2008/IFMAP/1" xmlns:meta="http://www.trustedcomputinggroup.org/2006/IFMAP-METADATA/1" xmlns:saml="urn:oasis:names:tc:SAML:2.0:assertion" xmlns:dsig="http://www.w3.org/2000/09/xmldsig#" xmlns:xenc="http://www.w3.org/2001/04/xmlenc#" xmlns:jnpr2="http://www.juniper.net/2009/IFMAP/1" xmlns:ifmap="http://www.trustedcomputinggroup.org/2006/IFMAP/1" xmlns:wsdl="http://www.juniper.net/2008/IFMAP/1/ifmap.wsdl">\
+    <SOAP-ENV:Header>\
+        <ifmap:session-id>' + sessionID + '</ifmap:session-id>\
+    </SOAP-ENV:Header>\
+    <SOAP-ENV:Body>\
+        <ifmap:publish validation="None">\
+        <delete>\
+        <identifier>\
+          <identity name="' + username + '" type="username" />\
+        </identifier>\
+      </delete>\
+            <update>\
+                <link>\
+                    <identifier>\
+                        <identity name="' + username + '" type="username" />\
+                    </identifier>\
+                    <identifier>\
+                        <ip-address value="1.1.1.1"/ type="IPv4">\
+                    </identifier>\
+                </link>\
+                <metadata>\
+                    <meta:role>\
+                        <name>Guest</name>\
+                    </meta:role>\
+                    <meta:role>\
+                        <name>Contractor</name>\
+                    </meta:role>\
+                </metadata>\
+            </update>\
+        </ifmap:publish>\
+    </SOAP-ENV:Body>\
+</SOAP-ENV:Envelope>';
+  return message;
+};
+
+exports.IfmapNode = IfmapNode;
--- server.js
+++ server.js
@@ -1,33 +1,21 @@
 var https = require('https');
 var parser = require('xml2json');
+var IfmapNode = require('./ifmap-node.js').IfmapNode;
 
 //evil globals
 var sessionID = '';
-
-var IfmapNode = function() {
-    this.info = '';
-};
-
-IfmapNode.constructor = IfmapNode;
-
-IfmapNode.prototype.getSession = function() {
-    var message = '<?xml version="1.0" encoding="UTF-8"?><SOAP-ENV:Envelope xmlns:SOAP-ENV="http://www.w3.org/2003/05/soap-envelope" xmlns:ifmap="http://www.trustedcomputinggroup.org/2006/IFMAP/1"><SOAP-ENV:Body><ifmap:new-session/> </SOAP-ENV:Body></SOAP-ENV:Envelope>';
-    return message;
-};
-
-IfmapNode.prototype.getUsers = function() {
-}
-
-IfmapNode.prototype.setUser = function(sessionID) {
-  var message = '<?xml version="1.0" encoding="UTF-8"?><SOAP-ENV:Envelope xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/" xmlns:SOAP-ENC="http://schemas.xmlsoap.org/soap/encoding/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:jnpr="http://www.juniper.net/2008/IFMAP/1" xmlns:meta="http://www.trustedcomputinggroup.org/2006/IFMAP-METADATA/1" xmlns:saml="urn:oasis:names:tc:SAML:2.0:assertion" xmlns:dsig="http://www.w3.org/2000/09/xmldsig#" xmlns:xenc="http://www.w3.org/2001/04/xmlenc#" xmlns:jnpr2="http://www.juniper.net/2009/IFMAP/1" xmlns:ifmap="http://www.trustedcomputinggroup.org/2006/IFMAP/1" xmlns:wsdl="http://www.juniper.net/2008/IFMAP/1/ifmap.wsdl"><SOAP-ENV:Header><ifmap:session-id>' + sessionID + '</ifmap:session-id></SOAP-ENV:Header><SOAP-ENV:Body><ifmap:publish validation="None"><update><link><identifier><identity name="DrBeef" type="username"/></identifier></link></update></ifmap:publish></SOAP-ENV:Body></SOAP-ENV:Envelope>';
-  return message;
-};
+//var soapPath = '/dana-ws/soap/dsifmap';
+var soapPath = '/';
+var soapPort = 8096;
+//var soapPort = 443;
+var soapHost = '10.0.1.250';
 
 var ifmapper = new IfmapNode();
 
 var options = {
-  host: '10.0.1.21',
-  path: '/dana-ws/soap/dsifmap',
+  host: soapHost,
+  port: soapPort,
+  path: soapPath,
   method: 'POST',
   auth: 'admin:hello',
   headers: {
@@ -44,32 +32,29 @@ var req = https.request(options, function(res) {
     console.log(sessionID);
     
     var options2 = {
-        host: '10.0.1.21',
-        path: '/dana-ws/soap/dsifmap',
+        host: soapHost,
+        port: soapPort,
+        path: soapPath,
         method: 'POST',
         auth: 'admin:hello',
         headers: {
                 'Content-Type': 'application/x-www-form-urlencoded',
-                'Content-Length': ifmapper.setUser(sessionID).length
+                'Content-Length': ifmapper.getUsers(sessionID).length
             }
       };
       
       var req2 = https.request(options2, function(res2) {
-      
         res2.on('data', function(d) {
           console.log(d.toString());
           var output = JSON.parse(parser.toJson(d.toString().replace(/(\w)[-]{1}(\w)/gi, '$1$2').replace(/(\w)[:]{1}(\w)/gi, '$1_$2')));
         });
-        
       });
       
       req2.on('error', function(e) {
         console.error(e);
       });
-      
-      // write data to request body
-      
-      req2.write(ifmapper.setUser(sessionID) + '\n');
+
+      req2.write(ifmapper.getUsers(sessionID,'DrBeef') + '\n');
       req2.end();
   });
   
